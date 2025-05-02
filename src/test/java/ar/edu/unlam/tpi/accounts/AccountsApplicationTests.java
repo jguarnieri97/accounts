2112e0df6d92015ad3dd402edd0127e7da2400a1
Julián Guarnieri <jguarnieri@alumno.unlam.edu.ar>
2025-05-02 19:36:42 -0300

    Base Project

---
+++ src/test/java/ar/edu/unlam/tpi/accounts/AccountsApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unlam.tpi.accounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
